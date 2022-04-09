c80274f20926a1d98e7f008f52acfcfaab3caaff
miho_kobayashi <m.koba17@gmail.com>
2022-04-09 08:38:45 +0000

    お気に入りの登録＆解除まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  # get 'favorites/create'
+  # get 'favorites/destroy'
   root 'tweets#index'
   
   # get 'tweets/new'
@@ -10,7 +12,11 @@ Rails.application.routes.draw do
   # get 'users/show'
   
   devise_for :users
-  resources :tweets
+  
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+  end
+   
   resources :users
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
